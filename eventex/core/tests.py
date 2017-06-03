ef28b6a74c3cf1c97e1656b8efafc650af32cfc4
Paulo Bueno <paulob.bruno@gmail.com>
2017-06-03 12:36:36 -0300

    Adiciona teste na home

--- eventex/core/tests.py
+++ eventex/core/tests.py
@@ -1,3 +1,20 @@
+# COMENTARIOS
+#
+# O ideal eh que tenhamos apenas um assert para cada funcao
+# cada funcao que comeca com 'test_' eh chamada de test method
+# cada funcao dentro de uma classe eh chamada de instancia
+
+
 from django.test import TestCase
 
-# Create your tests here.
+class HomeTest(TestCase):
+    def setUp(self):
+        self.response = self.client.get('/')
+
+    def test_get(self):
+        """ GET / must return status code 200 """
+        self.assertEqual(200, self.response.status_code)
+
+    def test_template(self):
+        """ Must use index.html """
+        self.assertTemplateUsed(self.response, 'index.html')
